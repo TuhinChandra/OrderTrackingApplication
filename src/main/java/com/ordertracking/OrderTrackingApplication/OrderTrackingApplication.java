096577d990952379e8264bc47167194d8cf4558c
Gourab Chakraborty <593040@TCS.com>
2020-06-01 15:46:22 +0530

    Initial code changes to convert it as Spring-Boot application

---
+++ src/main/java/com/ordertracking/OrderTrackingApplication/OrderTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.ordertracking.OrderTrackingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderTrackingApplication.class, args);
+	}
+
+}
