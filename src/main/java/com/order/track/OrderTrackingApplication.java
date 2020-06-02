a295b98d0898c86b4a506485d8e30d68ed29f778
chandt02 <chandt02@kits.kfplc.com>
2020-06-03 00:30:49 +0530

    Few modifications

--- src/main/java/com/order/track/OrderTrackingApplication.java
+++ src/main/java/com/order/track/OrderTrackingApplication.java
@@ -1,4 +1,4 @@
-package com.ordertracking.OrderTrackingApplication;
+package com.order.track;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
