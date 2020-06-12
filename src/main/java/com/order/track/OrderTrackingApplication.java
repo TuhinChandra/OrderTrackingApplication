d185aa63eb9bf57989191d72eeb90a36b2d2003e
Gourab Chakraborty <593040@TCS.com>
2020-06-12 18:07:36 +0530

    Few fixes

--- src/main/java/com/order/track/OrderTrackingApplication.java
+++ src/main/java/com/order/track/OrderTrackingApplication.java
@@ -2,12 +2,14 @@ package com.order.track;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class OrderTrackingApplication {
 
-	public static void main(final String[] args) {
-		SpringApplication.run(OrderTrackingApplication.class, args);
-	}
+    public static void main(final String[] args) {
+	SpringApplication.run(OrderTrackingApplication.class, args);
+    }
 
 }
